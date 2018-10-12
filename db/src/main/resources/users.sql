80144c02dd0fbe4c85fe93cce4671e3f8147a19e
Rajiv Jain <rajivpjs@gmail.com>
2018-10-12 06:43:59 +0100

    #6: correceted the name of the tables

--- db/src/main/resources/users.sql
+++ db/src/main/resources/users.sql
@@ -1,7 +1,7 @@
-create table user (
+create table users (
   emailAddress varchar(50),
   passw varchar(50),
   customerId int,
   PRIMARY KEY (emailAddress),
-  FOREIGN KEY (customerId) REFERENCES customer(customerId)
+  FOREIGN KEY (customerId) REFERENCES customers(customerId)
 );
