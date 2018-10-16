fd20a935d799cbafc1f6137aa7c4986a0debefeb
Rajiv Jain <rajivpjs@gmail.com>
2018-10-16 07:07:38 +0100

    #7: add Customer and User POJO

---
+++ customer/src/main/java/com/rppjs/customer/Customer.java
@@ -0,0 +1,9 @@
+package com.rppjs.customer;
+
+public class Customer {
+
+    private int customerId;
+    private String name;
+    private String surname;
+    private String emailAddress;
+}
---
+++ customer/src/main/java/com/rppjs/customer/User.java
@@ -0,0 +1,8 @@
+package com.rppjs.customer;
+
+public class User {
+
+    private String emailAddress;
+    private String password;
+    private int customerId;
+}
