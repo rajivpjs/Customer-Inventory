579c030f0ac29efab70ed3ff59bee3b1051984d7
Rajiv Jain <rajivpjs@gmail.com>
2018-10-17 07:07:32 +0100

    #7: add the JPA annotations

--- customer/src/main/java/com/rppjs/customer/Customer.java
+++ customer/src/main/java/com/rppjs/customer/Customer.java
@@ -1,9 +1,48 @@
 package com.rppjs.customer;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
 
-    private int customerId;
+    @Id
+    @GeneratedValue
+    private Long customerId;
     private String name;
     private String surname;
     private String emailAddress;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 }
--- customer/src/main/java/com/rppjs/customer/User.java
+++ customer/src/main/java/com/rppjs/customer/User.java
@@ -1,8 +1,37 @@
 package com.rppjs.customer;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
 public class User {
 
     private String emailAddress;
     private String password;
-    private int customerId;
+    @OneToOne
+    private Long customerId;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
 }
