371b53d34841f8cb503b97bbcd2b1e28f539482b
Rajiv Jain <rajivpjs@gmail.com>
2018-10-24 06:47:56 +0100

    #18: add CustomerRepository along with the application.properties file

---
+++ customer/src/main/java/com/rppjs/customer/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.rppjs.customer.repository;
+
+import com.rppjs.customer.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
