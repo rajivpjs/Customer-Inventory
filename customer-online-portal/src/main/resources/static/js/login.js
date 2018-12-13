0da74d04cd5076d5b6d195f3d474a4651331d0f8
Rajiv Jain <rajivpjs@gmail.com>
2018-12-13 07:37:47 +0000

    #44: add login changes

---
+++ customer-online-portal/src/main/resources/static/js/login.js
@@ -0,0 +1,24 @@
+$("#submit-login-form").on('click', function(e) {
+    var json = {
+            email : $("#login.email").val(),
+            pass  : $("#login.pass").val()
+            };
+
+    e.preventDefault();
+
+    $.ajax({
+           type: "POST",
+           url: "/login/customer",
+           data: JSON.stringify(json),
+           dataType: "json",
+           contentType : "application/json",
+           success: function(data)
+           {
+               document.getElementById("login-form").reset();
+               document.getElementById("login-title").innerHTML = "You have successfully logged in";
+           },
+           error: function(data) {
+               alert(data);
+           }
+         });
+});
