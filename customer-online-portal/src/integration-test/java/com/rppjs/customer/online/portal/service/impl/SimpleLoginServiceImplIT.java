f0253cfd2f6ee0d2a21e67c818773cefb36deb6b
Rajiv Jain <rajivpjs@gmail.com>
2018-12-19 07:22:46 +0000

    #49: add properties correctly

--- customer-online-portal/src/integration-test/java/com/rppjs/customer/online/portal/service/impl/SimpleLoginServiceImplIT.java
+++ customer-online-portal/src/integration-test/java/com/rppjs/customer/online/portal/service/impl/SimpleLoginServiceImplIT.java
@@ -2,6 +2,7 @@ package com.rppjs.customer.online.portal.service.impl;
 
 import com.rppjs.customer.online.portal.configuration.H2Configuration;
 import com.rppjs.customer.online.portal.configuration.LoginServiceConfiguration;
+import com.rppjs.customer.online.portal.configuration.MyConfig;
 import com.rppjs.customer.online.portal.entities.User;
 import com.rppjs.customer.online.portal.repository.UserRepository;
 import com.rppjs.customer.online.portal.service.LoginService;
@@ -10,12 +11,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {H2Configuration.class, LoginServiceConfiguration.class})
+@SpringBootTest(classes = {H2Configuration.class,
+        LoginServiceConfiguration.class, MyConfig.class})
 @Transactional
+@TestPropertySource("classpath:application-test.properties")
 public class SimpleLoginServiceImplIT {
 
     @Autowired
